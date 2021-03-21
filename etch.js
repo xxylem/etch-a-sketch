0dad6a0b27a9a19e65a38514d2a294a710a4e5fd
joshu <joshua.d.taylor@hotmail.co.uk>
2021-03-21 14:14:32 +0000

    Add prompt to reset grid with custom size. Center elements.

--- etch.js
+++ etch.js
@@ -1,39 +1,45 @@
-
-const getContainer = () => document.getElementById('container')
-
-const removeContainer = () => {
-    const oldContainer = getContainer();
-    if (oldContainer) oldContainer.remove();
-}
-
-const shadeCell = (e) => {
-
-}
-
-const createGrid = (nCols) => {
+const createNewGrid = (nCols) => {
     const totalCells = nCols * nCols;
     const newContainer = document.createElement('div');
     newContainer.id = 'container';
+
+    const shadeCell = (e) => {
+        e.target.classList.add('shaded-cell');
+    }
 
     for (let i = 0; i < totalCells; i++) {
         const cell = document.createElement('div');
         cell.className = 'cell';
 
-        cell.addEventListener('mouseover', (e) => {
-            cell.classList.add('shaded-cell');
-            console.log(this);
-        });
+        cell.addEventListener('mouseover', shadeCell);
         newContainer.appendChild(cell);
     }
 
     newContainer.style.gridTemplateColumns = `repeat(${nCols}, 1fr)`;
     newContainer.style.gridTemplateRows = `repeat(${nCols}, 1fr)`
 
+    const removeContainer = () => {
+        const oldContainer = document.getElementById('container');
+        if (oldContainer) oldContainer.remove();
+    }
+
     removeContainer();
     document.querySelector('body').appendChild(newContainer);
 }
 
-createGrid(4);
+const promptUserToReset = () => {
+    const nCols = parseInt(prompt('What size grid do you want? Enter a number between 1 and 100 to specify the number of rows and columns.'));
+
+    if (typeof nCols !== 'number') return;
+    if (nCols < 1 || nCols > 100) return;
+
+    createNewGrid(nCols);
+}
+
+const resetBtn = document.getElementById('reset-btn');
+resetBtn.addEventListener("click", promptUserToReset);
+
+createNewGrid(50);
 
 
 
